f6536335f5d728d7339da0c82acccf51688d7844
ahmedfinanceatoon-prog <ahmedfinanceatoon@gmail.com>
2026-08-03 21:56:57 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+ import * as XLSX from "xlsx";
+
+function exportTableToExcel(tableId, fileName) {
+  const table = document.getElementById(tableId);
+  const workbook = XLSX.utils.table_to_book(table, { sheet: "Sheet1" });
+  XLSX.writeFile(workbook, fileName + ".xlsx");
+}
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
